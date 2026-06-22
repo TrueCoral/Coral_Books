cbbfc0aad923d061a38d8aa9e0a0672f0e77ea9f
juan <juanconceicaodesouza@gmail.com>
2026-06-22 18:44:30 -0300

    Cadastros e Home

---
+++ Projeto-Site/Site/cadastro_leitor.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+<meta charset="UTF-8">
+<title>Cadastro Leitor</title>
+</head>
+
+<body>
+
+<h1>Cadastro de Leitor</h1>
+
+<form>
+
+<label>Nome</label>
+<input type="text">
+
+<br><br>
+
+<label>E-mail</label>
+<input type="email">
+
+<br><br>
+
+<label>Senha</label>
+<input type="password">
+
+<br><br>
+
+<button>
+Cadastrar
+</button>
+
+</form>
+
+</body>
+
+</html>
---
+++ Projeto-Site/Site/home.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+<meta charset="UTF-8">
+<title>Home</title>
+</head>
+
+<body>
+
+<h1>
+Seja bem-vindo ao Coral Books!
+</h1>
+
+</body>
+
+</html>

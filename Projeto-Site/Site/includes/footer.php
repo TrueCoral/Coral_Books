2559595ecbe63048ade2325752e687fc7bf6a40f
gugassis <gustavo.assis.calligaris@gmail.com>
2026-06-22 20:25:10 -0300

    includes

---
+++ Projeto-Site/Site/includes/footer.php
@@ -0,0 +1,8 @@
+<footer>
+<P>Coral Books</P>
+<p>Feito por Juan Conceição, Nickolas Lins e Gustavo Assis</p>
+<p>(19) 99576-9393</p>
+<p>micrurus.corallinustrue@gmail.com</p>
+<p>Insta: @TrueCoralBooks</p>
+
+</footer>

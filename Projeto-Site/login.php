f81a98e65f04c0996844be3fdf9153d8f369e57b
peixao <peixaodocv@gmail.com>
2026-06-22 19:19:26 -0300

    login

---
+++ Projeto-Site/login.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+<meta charset="UTF-8">
+<title>Login</title>
+</head>
+
+<body>
+
+<h1>Entrar</h1>
+
+<form>
+
+<label>E-mail</label>
+<input type="email">
+
+<br><br>
+
+<label>Senha</label>
+<input type="password">
+
+<br><br>
+
+<button>
+Entrar
+</button>
+
+</form>
+
+</body>
+
+</html>
